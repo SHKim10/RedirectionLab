6353dd7db95647f99e4e7ddd9afe91f8c603a795
Stacy <skim10@stuy.edu>
2020-12-23 13:25:39 -0500

    Implemented main that prints text as stars

---
+++ MakeStars.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class MakeStars{
+  public static void main( String[]args ){
+    Scanner read = new Scanner(System.in);
+    while(read.hasNextLine()){
+      String line = read.nextLine();
+      Scanner scanLine = new Scanner(line);
+      while (scanLine.hasNext()){
+        String stars = scanLine.next();
+        for (int i = 0; i < stars.length(); i++){
+          System.out.print("*");
+        }
+        System.out.println(" ");
+      }
+      System.out.println();
+    }
+  }
+}
